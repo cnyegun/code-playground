2581022c6a97d5896ee44ace2c58909e2d4b4e70
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-27 22:57:48 +0300

    changes

--- c-code/linked-list/main.c
+++ c-code/linked-list/main.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
 
-int main(
-)
+// DATA DEFENITION
+
+typedef struct {
+    void *next;
+    int data;
+} Node;
+
+// CONSTANTS DENIFNITION
+
+Node *head = NULL;
+
+// FUNCTIONS DEFENITION
+
+// LinkedList -> LinkedList
+// add a node to the list
+// example 
+
+// remove a node from the list
+
+// insert a node into a position in the list 
+
+void printMenu(){
+}
+
+int main(void)
+{
+    return 0;
+}
