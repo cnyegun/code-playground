dce090c5c1f5bf885b80f24e2ca169478615ab49
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-27 23:10:21 +0300

    changes

--- c-code/linked-list/main.c
+++ c-code/linked-list/main.c
@@ -3,7 +3,7 @@
 // DATA DEFENITION
 
 typedef struct {
-    void *next;
+    void *next = NULL;
     int data;
 } Node;
 
@@ -21,10 +21,36 @@ Node *head = NULL;
 
 // insert a node into a position in the list 
 
+// Display the program's menu to the user
 void printMenu(){
+    printf("You have the following options: \n");
+    printf("\t1. Add a note to the list.\n");
+    printf("\t2. Remove a note from the list.\n");
+    printf("\t3. Insert a note to the list.\n");
+    printf("\t4. Quit.\n");
 }
 
 int main(void)
 {
+    int option = -1;
+    while (option != 4)
+    {
+        printMenu();
+        int num_received = scanf("%d", &option);
+        if (num_received == 1 && option > 0 && option <= 4)
+        {
+            switch(option)
+            {
+                case 1:
+                    // add operation 
+                case 2:
+                    // remove operation
+                case 3:
+                    // insert operator
+                case 4:
+                    break;
+            }
+        }
+    }
     return 0;
 }
