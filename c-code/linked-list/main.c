11588d0af2a359659ac20ea886e78b28e7edb280
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-30 00:07:19 +0300

    changes

--- c-code/linked-list/main.c
+++ c-code/linked-list/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // DATA DEFENITION
-
-typedef struct {
-    void *next;
+typedef struct Node {
     int data;
+    struct Node *next;
 } Node;
 
 // CONSTANTS DENIFNITION
@@ -19,6 +19,13 @@ Node *head = NULL;
 // example    *head - NULL
 // add [4] -> *head - [4] - NULL
 void addOnHead();
+
+// Number -> Node 
+// create a node with the given number as its data
+Node* createNode(int value) {
+    Node* newNode = (Node*) malloc(sizeof(Node)); 
+    return newNode;
+}
 
 // remove a node from the list
 
@@ -35,25 +42,26 @@ void printMenu() {
 
 int main(void)
 {
-    int option = -1;
-    while (option != 4)
-    {
-        printMenu();
-        int num_received = scanf("%d", &option);
-        if (num_received == 1 && option > 0 && option <= 4)
-        {
-            switch(option)
-            {
-                case 1:
-                    // add operation 
-                case 2:
-                    // remove operation
-                case 3:
-                    // insert operator
-                case 4:
-                    break;
-            }
-        }
-    }
+    createNode(5);
+    // int option = -1;
+    // while (option != 4)
+    // {
+    //     printMenu();
+    //     int num_received = scanf("%d", &option);
+    //     if (num_received == 1 && option > 0 && option <= 4)
+    //     {
+    //         switch(option)
+    //         {
+    //             case 1:
+    //                 // add operation 
+    //             case 2:
+    //                 // remove operation
+    //             case 3:
+    //                 // insert operator
+    //             case 4:
+    //                 break;
+    //         }
+    //     }
+    // }
     return 0;
 }
